c873fc75bc46bacc0651adc4735283404cc66ad3
Robert Sorkin <robasorkin@gmail.com>
2016-10-08 11:44:23 +0300

    Add loging route and authenticate class method

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var bcrypt = require('bcryptjs');
 var _ = require('underscore');
 
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('user', {
+	return user = sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
@@ -40,11 +40,35 @@ module.exports = function(sequelize, DataTypes) {
 				}
 			}
 		},
+		classMethods: {
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
+					if (!_.isString(body.email) || !_.isString(body.password)) {
+						return reject();
+					}
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+							return reject();
+						}
+						resolve(user);
+
+
+					}, function(e) {
+						reject();
+					});
+				});
+			}
+		},
 		instanceMethods: {
 			toPublicJSON: function() {
 				var json = this.toJSON();
-				return  _.pick(json, 'id', 'createdAt', 'updatedAt', 'email');
+				return _.pick(json, 'id', 'createdAt', 'updatedAt', 'email');
 			}
 		}
 	});
+return user;
 };
