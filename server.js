59e97169cd223d6c236fc91468056bfad6cb8e99
Robert Sorkin <robasorkin@gmail.com>
2016-10-08 19:10:25 +0300

    heroku push

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ app.post('/users/login', function(req, res) {
 
 });
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function(req, res) {
 		console.log('Express listening on port ' + PORT + '!');
 	});
