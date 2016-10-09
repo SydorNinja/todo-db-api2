7679cbd37e54afda9902dfadbc6edbb94ffe8821
Robert Sorkin <robasorkin@gmail.com>
2016-10-09 20:09:33 +0300

    Adding DELETE /users/login

--- server.js
+++ server.js
@@ -137,21 +137,33 @@ app.post('/users', function(req, res) {
 
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
+
 	db.user.authenticate(body).then(function(user) {
 		var token = user.generateToken('authentication');
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());
-		} else {
-			res.status(401).send();
-		}
+		userInstance = user;
 
-	}, function(e) {
+		return db.token.create({
+			token: token
+		});
+	}).then(function(tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function() {
 		res.status(401).send();
 	});
-
 });
 
-db.sequelize.sync({force: true}).then(function() {
+app.delete('/users/login', middleware.requireAuthentication, function(req, res) {
+	req.token.destroy().then(function() {
+		res.status(204).send();
+	}, function() {
+		res.status(500).send();
+	});
+});
+
+db.sequelize.sync({
+	force: true
+}).then(function() {
 	app.listen(PORT, function(req, res) {
 		console.log('Express listening on port ' + PORT + '!');
 	});
